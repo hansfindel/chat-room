59ead427dd49cc2cf65582fd20f12ca887851c11
Hans <hanstrax@gmail.com>
2013-06-05 23:26:38 -0400

    troubles with the assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,4 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.initialize_on_precompile = false
 end
