685499b156448d0aaa24f00c225e966acc306ab9
Hans <hanstrax@gmail.com>
2013-06-05 23:55:59 -0400

    fixing redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,7 +4,7 @@ env_eq = (env == "production" )
 
 if env_eq 
 	uri = URI.parse(ENV["REDISTOGO_URL"])
-	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
 	#REDIS = Redis.new(:host => 'localhost', :port => 6379, :db => 0) 
 	$redis = Redis.new
